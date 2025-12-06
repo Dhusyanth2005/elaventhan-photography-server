a5685be63b87cc50e8ef57e5c1476c123f98abb4
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-12-06 14:51:04 +0530

    third  commit

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
 
+app.use(express.json());
+
+connectDB();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173','http://localhost:5174','https://elaventhan-photography.vercel.app','https://www.elaventhan-photography.vercel.app','https://admin-elaventhan-photography.vercel.app','https://www.admin-elaventhan-photography.vercel.app'],
@@ -12,19 +16,16 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
-
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/user', require('./routes/user'));
 
-// DB Connection
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Ranking System API is running on port ' + (process.env.PORT || 5000));
+});
+
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
