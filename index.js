eb29286d1b8ad108194d912d2e8c1516c60ae6df
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-12-06 14:28:04 +0530

    second commit

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
