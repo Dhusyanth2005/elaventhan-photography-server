c467a90ba126f0eeac8d493676900920f48da63f
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-12-06 15:44:23 +0530

    ninth commit

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
   }
 
   try {
-    const conn = await mongoose.connect('mongodb+srv://photodb:dbpassword@cluster0.bu1k4ok.mongodb.net/elaventhan_photography');
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = conn.connections[0].readyState;
     console.log("MongoDB Connected:", conn.connection.host);
--- index.js
+++ index.js
@@ -25,12 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Backend Running Successfully!');
 });
-app.get("/env-test", (req, res) => {
-  res.json({
-    mongo: process.env.MONGODB_URI,
-    jwt: process.env.JWT_SECRET
-  });
-});
+
 
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
